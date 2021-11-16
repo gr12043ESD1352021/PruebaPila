198451297037930e214dee31f85fc16d080e0f3a
chaic <chaic@DESKTOP-R9RR6C5>
2021-11-15 23:37:11 -0600

    Prueba arboles 20.00%

--- Pila.c
+++ Pila.c
@@ -1,6 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include "Pila.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+//4.1- Crear Nueva Persona
+
+Personas *CreandoPersona(int Dui, char Apellidos[50], char Nombres[50], int Edad, char Carrera[50]) {
+    Personas *NuevaPersona = (Personas*) malloc(sizeof (Personas));
+    NuevaPersona->Dui = Dui;
+    strcpy(NuevaPersona->Apellidos, Apellidos);
+    strcpy(NuevaPersona->Nombres, Nombres);
+    NuevaPersona->Edad = Edad;
+    strcpy(NuevaPersona->Carrera, Carrera);
+    return NuevaPersona;
+}
+
+//4.2- Creando Nodo
+
+Nodo* CreandoNodo(Personas* persona) {
+    Nodo *NuevoNodo = (Nodo*) malloc(sizeof (Nodo));
+    NuevoNodo->dato = persona;
+    NuevoNodo->Siguente = NULL;
+    return NuevoNodo;
+}
+
+//4.3- Creando Pila
+
+Pila* CreandoPila() {
+    Pila *NuevaPila = (Pila*) malloc(sizeof (Pila));
+    NuevaPila->cabeza = NULL;
+    NuevaPila->Contador = 0;
+    return NuevaPila;
+}
+
+//5.1- Validar si la pila esta vacia.
+
+int EstaVacia(Pila *pila) {
+    if (pila->Contador == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// 5.2- Agregar Nueva Persona
+
+void AgregarNuevaPersona(Pila *pila, Nodo *nodo) {
+    if (EstaVacia(pila)) {
+        pila->cabeza = nodo;
+    } else {
+        nodo->Siguente = pila->cabeza;
+        pila->cabeza = nodo;
+    }
+    pila->Contador++;
+}
+
+// 5.3- Imprimir pila
+
+void Imprimir(Pila *pila) {
+    Nodo *auxiliar = pila->cabeza;
+    Personas *aux;
+    while(auxiliar != NULL){
+        aux = auxiliar->dato;
+        printf("%d, %s, %s, %d, %s\n", auxiliar->dato->Dui, auxiliar->dato->Apellidos, auxiliar->dato->Nombres, auxiliar->dato->Edad, auxiliar->dato->Carrera);
+        auxiliar = auxiliar->Siguente;
+    }
+}
--- Pila.h
+++ Pila.h
@@ -7,9 +7,58 @@
 extern "C" {
 #endif
 
+    /*Para crear una pila necesitamos:
+     1- La estrucura de los datos que contendra la app.
+     2- Estructura Nodo con referencia a la estrucutura los datos.
+     3- Estructura pila que tenga referencia con Nodo que cuente la cantidad de datos
+     4- Metodos para crear Estrucutras: de datos, nodo y pila.
+     5- Metodos que permitan jugar con los datos de la pila.
+     */
 
+    //Declaraciones
+    //1- Estructura base.
 
+    typedef struct Persona {
+        int Dui;
+        char Apellidos[50];
+        char Nombres[50];
+        int Edad;
+        char Carrera[30];
+    } Personas;
 
+    //2- Estructura Nodo
+
+    typedef struct nodo {
+        Personas *dato;
+        struct nodo *Siguente;
+    } Nodo;
+
+    //3- Estructura pila
+
+    typedef struct pila {
+        int Contador;
+        Nodo *cabeza;
+    } Pila;
+
+    //4- Creando metodos de estrucutras
+    //4.1- Creando Nueva persona
+    Personas *CreandoPersona(int Dui, char Apellido[50], char Nombre[50], int Edad, char Carrera[50]);
+
+    //4.2- Creando Nodo
+    Nodo* CreandoNodo(Personas* persona);
+
+    //4.3- Creando Pila
+    Pila* CreandoPila(Personas *persona, Nodo *nodo);
+    
+    //5- Creando metodos para jugar con los datos.
+    //5.1- Validar si la pila esta vacia.
+    int EstaVacia(Pila *pila);
+    
+    // 5.2- Agregar Nueva Persona
+    void AgregarNuevaPersona(Pila *pila, Nodo *nodo);
+    
+    // 5.3- Imprimir pila-
+    void Imprimir(Pila *pila);
 #ifdef __cplusplus
 }
 #endif
